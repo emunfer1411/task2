72a99d01c6290b54681c6a5ecb64a9ac6e7660e3
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-10 12:20:44 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
-basic.pause(100)
-maqueen.motorStop(maqueen.Motors.M1)
-basic.pause(100)
+basic.pause(3000)
+maqueen.motorStop(maqueen.Motors.All)
+basic.pause(2000)
 maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
-basic.pause(100)
+basic.pause(3000)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
 basic.forever(function () {
 	
