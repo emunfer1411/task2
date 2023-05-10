f44c46e65aa48c5c382295aa18f9e45708f9cafd
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-10 12:01:58 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+basic.pause(100)
+maqueen.motorStop(maqueen.Motors.M1)
+basic.pause(100)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+basic.pause(100)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+basic.forever(function () {
+	
+})
